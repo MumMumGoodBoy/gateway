8699e5d747ce3c3d921d3cd5fe0c8be72fdafc63
oOWinOo <peetwin011@hotmail.co.th>
2024-11-09 12:26:46 +0700

    fix(search) : add max and min price filter for food

--- internal/handler/search/search.go
+++ internal/handler/search/search.go
@@ -30,7 +30,9 @@ func (h *SearchHandler) SearchFoods(c *fiber.Ctx) error {
 	searchQuery := c.Query("search")
 	offset := c.Query("offset")
 	limit := c.Query("limit")
+	maxPrice := c.Query("maxPrice")
+	minPrice := c.Query("minPrice")
 
-	url := fmt.Sprintf("%s/search/foods?search=%s&offset=%s&limit=%s", h.cfg.SearchConfig.SearchServiceAddr, searchQuery, offset, limit)
+	url := fmt.Sprintf("%s/search/foods?search=%s&offset=%s&limit=%s&maxPrice=%s&minPrice=%s", h.cfg.SearchConfig.SearchServiceAddr, searchQuery, offset, limit, maxPrice, minPrice)
 	return api.HandleRedirect(url, c)
 }
